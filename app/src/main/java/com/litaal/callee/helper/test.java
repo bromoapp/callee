89cbd781f96ba17a538238e335a1ee0894646fdf
bromokun <bromokun@gmail.com>
2017-06-04 18:48:37 +0700

    updates

--- app/src/main/java/com/litaal/callee/helper/test.java
+++
@@ -1,8 +0,0 @@
-package com.litaal.callee.helper;
-
-/**
- * Created by sadmin on 6/4/2017.
- */
-
-public class test {
-}
--- app/src/main/java/com/litaal/callee/service/bg/test.java
+++
@@ -1,8 +0,0 @@
-package com.litaal.callee.service.bg;
-
-/**
- * Created by sadmin on 6/4/2017.
- */
-
-public class test {
-}
